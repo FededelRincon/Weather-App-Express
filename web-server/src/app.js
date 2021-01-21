2bc3143259608fca9ffebdc22e987027aa902554
“fededelrincon” <delrincon@gmail.com>
2021-01-21 18:28:49 -0300

    044- Serving html and json as response page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,21 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page !')
+    res.send([{
+        name: 'Andrew',
+        age: 27
+    }, {name: 'Sarah'}])
 })
 
+
+
 app.get('/about', (req, res) => {
-    res.send('About page!')
+    res.send('<h1> About </h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page!')
+    res.send({
+        forecast: 'it is snowing',
+        location: 'Philadelphia'
+    })
 })
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
+    //http://localhost:3000/about
+    //http://localhost:3000/help
 })
