b2d589ffb4f27c090b246dd55f9b838ba20d8159
“fededelrincon” <delrincon@gmail.com>
2021-01-21 19:00:38 -0300

    045- Serving up Static assets. Index.html, about.html, help.html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Andrew',
-        age: 27
-    }, {name: 'Sarah'}])
-})
+app.use(express.static(publicDirectoryPath))
+//http://localhost:3000/
+//http://localhost:3000/help  //estas funcionan xq puse el path mirando a esta carpeta, y existen los html en ese lugar.
+//http://localhost:3000/about
+//http://localhost:3000/weather
 
 
-
-app.get('/about', (req, res) => {
-    res.send('<h1> About </h1>')
-})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -28,6 +21,4 @@ app.get('/weather', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
-    //http://localhost:3000/about
-    //http://localhost:3000/help
 })
