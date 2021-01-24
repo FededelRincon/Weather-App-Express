56e288673d07769f486e9758ff5a78c4186d66a9
“fededelrincon” <delrincon@gmail.com>
2021-01-24 10:19:19 -0300

    055- add geocode and forecast to weather route

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -48,12 +50,26 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address terms'
         })
     }
+    //http://localhost:3000/weather?address=Boston
 
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia',
-        address: `${queryAddress}`,
+    geocode(queryAddress, (error, { latitude, longitude, location }) => {
+        if(error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: queryAddress
+            })
+        })
     })
+
 })
 
 app.get('/products', (req, res) => {
